3e5d447dd1b18a8caf9834c3430749d77d17b91b
L <samuel.zhengqinlai@gmail.com>
2022-01-21 15:45:52 +0800

    test 1 in test_1 branch

--- IDEA X Git/src/Test.java
+++ IDEA X Git/src/Test.java
@@ -1,6 +1,6 @@
 public class Test {
     public static void main(String[] args){
         System.out.println("12");
-        System.out.println("123453");
+        System.out.println("1233");
     }
 }
